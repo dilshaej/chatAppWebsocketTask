a22d49abbb9520e99c7913158c617fc680812dce
dilshaej <dilshaejaleel5555@gmail.com>
2024-03-29 17:23:25 +0530

    first commit

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,11 +1,16 @@
+// src/websocket.js
+
+// Determine the host based on the environment
 const host = process.env.NODE_ENV === 'production' ? window.location.host.replace(/^http/, 'ws') : 'ws://localhost:5173';
 
 export let send;
 let onMessageCallback;
 
 export const startWebsocketConnection = () => {
+  // Initialize WebSocket connection
   const ws = new WebSocket(host + '/chat'); // Make sure '/chat' is the correct endpoint on your server
 
+  // WebSocket event handlers
   ws.onopen = () => {
     console.log('opened ws connection');
   };
@@ -15,12 +20,15 @@ export const startWebsocketConnection = () => {
   };
 
   ws.onmessage = (e) => {
+    // Call registered message callback when a message is received
     onMessageCallback && onMessageCallback(e.data);
   };
 
+  // Bind send method to exported variable
   send = ws.send.bind(ws);
 };
 
+// Register callback function to handle incoming messages
 export const registerOnMessageCallback = (fn) => {
   onMessageCallback = fn;
 };
