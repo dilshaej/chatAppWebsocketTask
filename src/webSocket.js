89b40efcfd982d613ddccac128c40376c1c17abb
dilshaej <dilshaejaleel5555@gmail.com>
2024-03-29 15:23:19 +0530

    first commit

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,46 +1,51 @@
 // src/websocket.js
 
-// Create a queue to hold messages while the WebSocket connection is still connecting
-const messageQueue = [];
+// The server host is determined based on the mode
+// If the app is running in development mode (using npm start)
+// then we set the host to "localhost:8080"
+// If the app is in production mode (using npm run build)
+// then the host is the current browser host
+const host = process.env.NODE_ENV === 'production' ? window.location.host : 'localhost:5173'
 
-let ws; // Declare ws outside the function to make it accessible globally
+// We create an exported variable send, that we will assign
+// later (just know that it is exported for now)
+export let send
+// The onMessageCallback is also assigned later, as we will soon see
+let onMessageCallback
 
+// This exported function is used to initialize the websocket connection
+// to the server
 export const startWebsocketConnection = () => {
-  ws = new WebSocket('wss://' + host + '/chat'); // Use wss:// instead of ws://
+  // A new Websocket connection is initialized with the server
+  const ws = new window.WebSocket('ws://' + host + '/chat') || {}
 
+  // If the connection is successfully opened, we log to the console
   ws.onopen = () => {
-    console.log('opened ws connection');
+    console.log('opened ws connection')
+  }
 
-    // Send any queued messages
-    while (messageQueue.length > 0) {
-      const message = messageQueue.shift();
-      ws.send(JSON.stringify(message));
-    }
-  };
-
-  ws.onmessage = (e) => {
-    onMessageCallback && onMessageCallback(e.data);
-  };
-
+  // If the connection is closed, we log that as well, along with
+  // the error code and reason for closure
   ws.onclose = (e) => {
-    console.log('close ws connection: ', e.code, e.reason);
-  };
+    console.log('close ws connection: ', e.code, e.reason)
+  }
 
-  send = (message) => {
-    // If WebSocket is not open, queue the message
-    if (ws.readyState !== WebSocket.OPEN) {
-      console.log('WebSocket connection is not open yet. Queuing message:', message);
-      messageQueue.push(message);
-    } else {
-      ws.send(message);
-    }
-  };
-};
+  // This callback is called everytime a message is received.
+  ws.onmessage = (e) => {
+    // The onMessageCallback function is called with the message
+    // data as the argument
+    onMessageCallback && onMessageCallback(e.data)
+  }
 
+  // We assign the send method of the connection to the exported
+  // send variable that we defined earlier
+  send = ws.send.bind(ws)
+}
+
+// This function is called by our React application to register a callback
+// that needs to be called everytime a new message is received
 export const registerOnMessageCallback = (fn) => {
   // The callback function is supplied as an argument and assigned to the 
   // onMessageCallback variable we declared earlier
   onMessageCallback = fn
-};
-
-export const send = ws.send.bind(ws);
+}
